83d4c4f2f29c8bce48faacb41b7ae172bd28b69a
Vinayak kamati <vinayakkamati123@gmail.com>
2023-01-18 12:11:01 +0530

    Initial commit: created project repo

---
+++ spring-voting-system/src/main/java/com/assignment/springvotingsystem/SpringVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.springvotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVotingSystemApplication.class, args);
+	}
+
+}
---
+++ spring-voting-system/src/test/java/com/assignment/springvotingsystem/SpringVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.springvotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
